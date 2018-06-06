86328205fb5796b5153ee3453a08de950ecca9ab
Akash <akash.darknight@gmail.com>
2018-06-06 13:03:00 +0530

    Art Generator

--- Neural_Art_Transfer.py
+++ Neural_Art_Transfer.py
@@ -47,3 +47,12 @@ def compute_content_cost(a_C, a_G):
 
     return J_content
 
+tf.reset_default_graph()
+
+with tf.Session() as test:
+    tf.set_random_seed(1)
+    a_C = tf.random_normal([1, 4, 4, 3], mean=1, stddev=4)
+    a_G = tf.random_normal([1, 4, 4, 3], mean=1, stddev=4)
+    J_content = compute_content_cost(a_C, a_G)
+    print("J_content = " + str(J_content.eval()))
+
