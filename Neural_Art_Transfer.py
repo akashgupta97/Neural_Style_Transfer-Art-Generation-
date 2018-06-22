1e3bb32dfdbc00cd16ab1f88614e84d5f38b37f2
Akash <akash.darknight@gmail.com>
2018-06-22 13:03:00 +0530

    Art Generator

--- Neural_Art_Transfer.py
+++ Neural_Art_Transfer.py
@@ -157,6 +157,26 @@ with tf.Session() as test:
         # initialize the overall style cost
         J_style = 0
 
+        for layer_name, coeff in STYLE_LAYERS:
+            # Select the output tensor of the currently selected layer
+            out = model[layer_name]
+
+            # Set a_S to be the hidden layer activation from the layer we have selected, by running the session on out
+            a_S = sess.run(out)
+
+            # Set a_G to be the hidden layer activation from same layer. Here, a_G references model[layer_name]
+            # and isn't evaluated yet. Later in the code, we'll assign the image G as the model input, so that
+            # when we run the session, this will be the activations drawn from the appropriate layer, with G as input.
+            a_G = out
+
+            # Compute style_cost for the current layer
+            J_style_layer = compute_layer_style_cost(a_S, a_G)
+
+            # Add coeff * J_style_layer of this layer to overall style cost
+            J_style += coeff * J_style_layer
+
+        return J_style
+
 
 
 
