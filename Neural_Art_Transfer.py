851f36d59494587d871e55654fc7c486b4425f3f
Akash <akash.darknight@gmail.com>
2018-06-24 13:03:00 +0530

    Art Generator

--- Neural_Art_Transfer.py
+++ Neural_Art_Transfer.py
@@ -178,6 +178,25 @@ with tf.Session() as test:
         return J_style
 
 
+    # GRADED FUNCTION: total_cost
+
+    def total_cost(J_content, J_style, alpha=10, beta=40):
+        """
+        Computes the total cost function
+
+        Arguments:
+        J_content -- content cost coded above
+        J_style -- style cost coded above
+        alpha -- hyperparameter weighting the importance of the content cost
+        beta -- hyperparameter weighting the importance of the style cost
+
+        Returns:
+        J -- total cost as defined by the formula above.
+        """
+
+        
+
+
 
 
 
