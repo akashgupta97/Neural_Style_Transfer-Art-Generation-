8d4f9f6b89dcd2dea49bf65bf1e65c9e5fd00b70
Akash <akash.darknight@gmail.com>
2018-07-21 13:03:00 +0530

    Art Generator

--- Neural_Art_Transfer.py
+++ Neural_Art_Transfer.py
@@ -274,7 +274,33 @@ with tf.Session() as test:
         sess.run(model['input'].assign(input_image))
         ### END CODE HERE ###
 
-        
+        for i in range(num_iterations):
+
+            # Run the session on the train_step to minimize the total cost
+            ### START CODE HERE ### (1 line)
+            sess.run(train_step)
+            ### END CODE HERE ###
+
+            # Compute the generated image by running the session on the current model['input']
+            ### START CODE HERE ### (1 line)
+            generated_image = sess.run(model['input'])
+            ### END CODE HERE ###
+
+            # Print every 20 iteration.
+            if i % 20 == 0:
+                Jt, Jc, Js = sess.run([J, J_content, J_style])
+                print("Iteration " + str(i) + " :")
+                print("total cost = " + str(Jt))
+                print("content cost = " + str(Jc))
+                print("style cost = " + str(Js))
+
+                # save current generated image in the "/output" directory
+                save_image("output/" + str(i) + ".png", generated_image)
+
+                # save last generated image
+        save_image('output/generated_image.jpg', generated_image)
+
+        return generated_image
 
 
 
